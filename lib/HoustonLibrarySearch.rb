6a9b5ca6598b54d9fefb49d226235bf62e9d3eb2
Nelson Morris <nmorris@nelsonmorris.net>
2010-02-22 18:00:21 -0600

    Remove Bundler usage.

--- lib/HoustonLibrarySearch.rb
+++ lib/HoustonLibrarySearch.rb
@@ -1,13 +1,3 @@
-begin
-  # Try to require the preresolved locked set of gems.
-  require File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fall back on doing an unlocked resolve at runtime.
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-end
-
 require "curb"
 require "nokogiri"
 require File.join(File.expand_path(File.dirname(__FILE__)), "listing.rb")
