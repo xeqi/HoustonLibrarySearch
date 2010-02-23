71d2e265729eed2699b862198ceb57482e4f31bb
Nelson Morris <nmorris@nelsonmorris.net>
2010-02-22 19:27:52 -0600

    Follow ruby library pattern of a file with a bunch of requires

--- lib/HoustonLibrarySearch.rb
+++ lib/HoustonLibrarySearch.rb
@@ -1,27 +1,6 @@
 require "curb"
 require "nokogiri"
-require File.join(File.expand_path(File.dirname(__FILE__)), "listing.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "HoustonLibrarySearch/listing.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "HoustonLibrarySearch/isbn.rb")
 
 Dir[File.join(File.expand_path(File.dirname(__FILE__)), "plugins/*.rb")].each {|file| require file }
-
-class HoustonLibrarySearch
-  def self.search(isbn)
-    multi = Curl::Multi.new
-    results = {}
-    [HPL, HCPL].each do |library|
-      c = Curl::Easy.new(library.url(isbn)) do |curl|
-        curl.on_success do |data|
-          #TODO Handle parse errors
-          #TODO Add mechanism for multiple page responses
-          results.merge!({library.name => library.parse(data.body_str)})
-        end
-      end
-      multi.add(c)
-    end
-
-    multi.perform do
-      #TODO Include a timeout
-    end
-    results
-  end
-end
---
+++ lib/HoustonLibrarySearch/isbn.rb
@@ -0,0 +1,23 @@
+module HoustonLibrarySearch
+  class ISBN 
+    def self.search(isbn)
+      multi = Curl::Multi.new
+      results = {}
+      [HPL, HCPL].each do |library|
+        c = Curl::Easy.new(library.url(isbn)) do |curl|
+          curl.on_success do |data|
+            #TODO Handle parse errors
+            #TODO Add mechanism for multiple page responses
+            results.merge!({library.name => library.parse(data.body_str)})
+          end
+        end
+        multi.add(c)
+      end
+  
+      multi.perform do
+        #TODO Include a timeout
+      end
+      results
+    end
+  end
+end
